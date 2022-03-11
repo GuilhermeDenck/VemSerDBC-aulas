85382d0b388b777a3d17d4949b480d2a71853a62
Guilherme Denck <guidenck0303@gmail.com>
2022-03-11 00:00:53 -0300

    fix: script5

--- JavaScript/pair-programming/script5.js
+++ JavaScript/pair-programming/script5.js
@@ -3,17 +3,29 @@
 //   => possiveis combinações da string "tri":  
 //   't', 'tr', 'ti', 'tri', 'tir', 'r', 'rt', 'ri', 'rit', 'rti', 'i', 'ir', 'it', 'irt', 'itr'
 
-const imprimirCombinacoes = string => {
-  let possibilidades = [];
-  for(let i = 0; i < string.length; i++) {
-    let letra = string[i];
-    let letras = string.split(letra);
-    for(let j = 0; j < letras.length; j++) {
-      let possibilidade = letra + letras[j];
-      possibilidades.push(possibilidade);
+const imprimirCombinacoes = str => {
+  let arrayStr = str.split('');
+  let arrayCombinations = [];
+  let aux = '';
+  for(let i = 0; i < arrayStr.length; i++) {
+    aux += arrayStr[i];
+    arrayCombinations.push(aux)
+    for(let j = 0; j < arrayStr.length; j++) {
+      if(i != j) {
+        aux += arrayStr[j]
+        arrayCombinations.push(aux)
+      }
     }
+    aux = arrayStr[i]
+    for(let k = (arrayStr.length - 1); k >= 0; k--) {
+      if(i != k) {
+        aux += arrayStr[k]
+        arrayCombinations.push(aux)
+      }
+    }
+    aux = '';
   }
-  return possibilidades;
+  return arrayCombinations;
 }
 
 console.log(imprimirCombinacoes("tri"));

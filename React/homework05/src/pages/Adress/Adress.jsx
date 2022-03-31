a7f4ad555cc33a8169955c415b5beba87245a802
Guilherme Denck <guidenck0303@gmail.com>
2022-03-30 23:01:54 -0300

    feat/refazendo form

--- React/homework05/src/pages/Adress/Adress.jsx
+++ React/homework05/src/pages/Adress/Adress.jsx
@@ -1,22 +1,31 @@
-import { Formik, Field, Form } from 'formik';
+import { useState } from 'react';
 import axios from 'axios';
 
 import style from './Adress.module.css';
 
 function Adress() {
 
-  const handleCep = async(values, setFieldValue) => {
-    const { value } = values.target;
-    if(values.target.value.length === 9) {
+  const [cep, setCep] = useState('');
+  const [logradouro, setLogradouro] = useState('');
+  const [complemento, setComplemento] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [localidade, setLocalidade] = useState('');
+  const [uf, setUf] = useState('');
+  const [tel, setTel] = useState('');
+
+  const handleCep = async(e) => {
+    let finalCep = e.target.value.split('-').join('');
+    if(finalCep.length === 8) {
       try {
-        const finalCep = value.split('-').join('');
         const {data} = await axios.get(`https://viacep.com.br/ws/${finalCep}/json/`);
-        setFieldValue('rua', data.logradouro);
-        setFieldValue('complemento', data.complemento);
-        setFieldValue('bairro', data.bairro);
-        setFieldValue('cidade', data.localidade);
-        setFieldValue('estado', data.uf);
-        setFieldValue('tel', data.ddd);
+        console.log(data);
+        setLogradouro(data.logradouro)
+        setComplemento(data.complemento)
+        setBairro(data.bairro)
+        setLocalidade(data.localidade)
+        setUf(data.uf)
+        setTel(data.ddd)
+        
         return data;
       } catch (error) {
         alert('CEP não encontrado');
@@ -24,78 +33,77 @@ function Adress() {
       }
     }
   }
-
-  const validateCep = (value) => {
-    let error;
-    if (!value) {
-      error = 'Required';
-    } else if (/[1-9]/g.test(value)) {
-      error = 'Invalid cep address';
-    }
-    return error;
-  }
-
+  
   const regexOnlyNumbers = /\D/g;
 
-  const maskCep = (e, setFieldValue ) => {
-    setFieldValue('cep', e.target.value.replace(regexOnlyNumbers, '').replace(/(\d{5})(\d)/, '$1-$2'));
+  const maskCep = (e) => {
+    setCep(e.target.value.replace(regexOnlyNumbers, '').replace(/(\d{5})(\d{3})/, '$1-$2'));
+    return e;
   }
 
-  const maskTel = (e, setFieldValue ) => {
-    setFieldValue('tel', e.target.value.replace(regexOnlyNumbers, '').replace(/(\d{2})(\d)/, "($1) $2").replace(/(\d{5})(\d{4})(\d)/, "$1-$2"));
+  const maskTel = (e) => {
+    setTel(e.target.value.replace(regexOnlyNumbers, '').replace(/(\d{2})(\d)/, "($1) $2").replace(/(\d{5})(\d{4})(\d)/, "$1-$2"));
+    return e;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const obj = {
+      cep: cep,
+      logradouro: logradouro,
+      complemento: complemento,
+      bairro: bairro,
+      localidade: localidade,
+      uf: uf,
+      tel: tel
+    }
+    alert('Dados enviados com sucesso');
+    alert(JSON.stringify(obj, null, 2));
+
+    setCep('');
+    setLogradouro('');
+    setComplemento('');
+    setBairro('');
+    setLocalidade('');
+    setUf('');
+    setTel('');
   }
 
   return (
     <div className={style.pageAdress}>
       <div className={style.containerCep}>
         <h1>Cep</h1>
-        <Formik
-          initialValues={{
-            cep: '',
-            rua: '',
-            complemento: '',
-            bairro: '',
-            cidade: '',
-            estado: '',
-            tel: ''
-          }}
-          onSubmit={() => {
-            alert('Cadastrado com sucesso');
-          }}
-          render={({ setFieldValue }) => (
-            <Form className={style.form}>
-              <div>
-                <label htmlFor="cep">Cep</label>
-                <Field id="cep" name="cep" validate={validateCep} maxLength="9" placeholder="digite seu CEP" onBlur={ e => handleCep(e, setFieldValue)} onChange={ e => maskCep(e, setFieldValue)} />
-              </div>
-              <div>
-                <label htmlFor="rua">Rua</label>
-                <Field id="rua" name="rua" placeholder="digite sua Rua" />
-              </div>
-              <div>
-                <label htmlFor="complemento">Complemento</label>
-                <Field id="complemento" name="complemento" placeholder="digite seu Complemento" />
-              </div>
-              <div>
-                <label htmlFor="bairro">Bairro</label>
-                <Field id="bairro" name="bairro" placeholder="digite seu Bairro" />
-              </div>
-              <div> 
-                <label htmlFor="cidade">Cidade</label>
-                <Field id="cidade" name="cidade" placeholder="digite sua Cidade" />
-              </div>
-              <div>
-                <label htmlFor="estado">Estado</label>
-                <Field id="estado" name="estado" placeholder="digite seu Estado" />
-              </div>
-              <div>
-                <label htmlFor="tel">Telefone</label>
-                <Field id="tel" name="tel" placeholder="digite sua Telefone" maxLength="15" onChange={ e => maskTel(e, setFieldValue)} />
-              </div>
-              <button type="submit">Cadastrar</button>
-            </Form>
-          )}
-        />
+          <form onSubmit={handleSubmit} className={style.form}>
+            <div>
+              <label htmlFor="cep">Digite seu Cep</label>
+              <input type="text"  name="cep" value={cep} maxLength="9" placeholder="digite seu CEP" onChange={ e => handleCep(maskCep(e))}/>
+            </div>
+            <div>
+              <label htmlFor="rua">Rua</label>
+              <input type="text" id="rua" value={logradouro} required placeholder="digite sua Rua" onChange={ e => setLogradouro(e.target.value)}/>
+            </div>
+            <div>
+              <label htmlFor="complemento">Complemento</label>
+              <input type="text" id="complemento" value={complemento} placeholder="digite sua Complemento" onChange={ e => setComplemento(e.target.value)}/>
+            </div>
+            <div>
+              <label htmlFor="bairro">Bairro</label>
+              <input type="text" id="bairro" value={bairro} required placeholder="digite seu bairro" onChange={ e => setBairro(e.target.value)}/>
+            </div>
+            <div>
+              <label htmlFor="cidade">Cidade</label>
+              <input type="text" id="cidade" value={localidade} required placeholder="digite sua Cidade" onChange={ e => setLocalidade(e.target.value)}/>
+            </div>
+            <div>
+              <label htmlFor="estado">Estado</label>
+              <input type="text" id="estado" value={uf} required placeholder="digite seu Estado" onChange={ e => setUf(e.target.value)}/>
+            </div>
+            <div>
+              <label htmlFor="tel">Telefone</label>
+              <input type="text" id="tel" value={tel} placeholder="digite seu Telefone" onChange={ e => maskTel(e)} />
+            </div>
+            <button type="submit">Cadastrar</button>
+          </form>
       </div>
     </div>
   );
